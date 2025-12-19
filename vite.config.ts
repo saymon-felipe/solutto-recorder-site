6b800f5b07b69fccdc75a55c8f17d2af80b0c7f9
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-12-18 21:24:34 -0300

    Retirando alias do vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -8,11 +7,6 @@ export default defineConfig(() => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
+      plugins: [react()]
     };
 });
